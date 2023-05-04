c9d9ecc5160a6db831800a24fa0327ae3d382506
georgieannett <georgie.ap00@gmail.com>
2023-05-04 22:24:33 +0100

    Started Attempt to link to Frontend via FLASK

---
+++ Frontend_attempt.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+# Flask - new Flask web application
+# render_template - This renders the template for the web pages
+# request - get data from the user's request
+# redirect - redirect to a different web page
+# url_for - generates a URL to a specific function
+# session - store user-specific data.
+#
+
+import sqlite3
+
+# python library that allows you to store and retrieve data from an SQL database.
+
+app = Flask(__name__)  # creates a new Flask application
+db_path = 'CFGfinalprojectdatabase.db' # assigns the string value to CFG final project DB
+
