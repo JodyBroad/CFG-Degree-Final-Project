b2750834bde71a394877d50a3d84c5609ae10003
Jody Broad <msjodybroad@gmail.com>
2023-05-06 14:49:10 +0100

    added additional route to see current db contents

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Email, DataRequired
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@localhost/CFGFinalProject"
 app.config['SECRET_KEY'] = "random string"
 
 db = SQLAlchemy(app)
 
+
+# This should be in separate models.py file
 # Models - tables for the db
 
 # UserInfo table
@@ -27,20 +30,25 @@ def __init__(self, forename, surname, email):
    self.email = email
 
 
+
+# This should be in separate create.py file
 # Values for the table
 #
 # UserInfo table
 # each row of the table
 user_1 = UserInfo(forename='Jody', surname='Broad', email='Jody@email.com')
 user_2 = UserInfo(forename='Melissa', surname='Long', email='Melissa@email.com')
+user_3 = UserInfo(forename='Rada', surname='Kanchananupradit', email='Rada@email.com')
+user_4 = UserInfo(forename='Khadija', surname='Warsama', email='Khadija@email.com')
+user_5 = UserInfo(forename='Georgie', surname='Annett', email='Georgie@email.com')
 # save the rows to a list
-users = [user_1, user_2]
+users = [user_1, user_2, user_3, user_4, user_5]
 
 # need to create all the other tables!
 
 
 
-
+# This should be in separate forms.py file
 # forms
 
 
@@ -52,6 +60,8 @@ class BasicRegistrationForm(FlaskForm):
     submit = SubmitField('Register')
 
 
+
+# This should be in separate routes.py file
 # routes
 
 @app.route('/home', methods=['GET', 'POST'])
@@ -70,17 +80,28 @@ def home():
             user = UserInfo(forename=forename, surname=surname, email=email)
             db.session.add(user)
             db.session.commit()
-            # this flash doesn't work yet as I haven't set up a navbar or anything
             flash(f'Success! {form.email.data} is now signed up for an account', 'success')
             return render_template('home.html', form=form, message=error, title='home')
     return render_template('home.html', form=form, message=error, title='home')
 
-
-
-
+# getting list of current users
+@app.route('/user_list', methods=['GET'])
+def user_list():
+    error = ""
+    users = UserInfo.query
+    # if len(users) == 0:
+    #     error = "There are no people to display"
+    #     print(users)
+    return render_template('user_list.html', users=users, message=error)
 
 
 if __name__ == '__main__':
     with app.app_context():
+        # just drops any existing data
+        db.drop_all()
+        # creates all the tables
         db.create_all()
+        # use this to add the list of users all at once (will add more for other tables)
+        db.session.add_all(users)
+        db.session.commit()
     app.run(debug=True)
