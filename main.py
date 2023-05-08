fce166fe101bd9f4fb3627383259b0b8269fa9ad
mellon11 <melissajlong5@gmail.com>
2023-05-08 18:46:16 +0100

    move sleep-tracker endpoints to main

--- main.py
+++ main.py
@@ -92,6 +92,16 @@ def user_list():
     users = UserInfo.query
     return render_template('user_list.html', users=users, message=error)
 
+@app.route('/sleep-tracker', methods=['GET'])
+def sleep_tracker():
+    return render_template('sleep-tracker.html')
+
+@app.route('/api/sleep-tracker/log-sleep', methods=['POST'])
+def log_sleep():
+    # to do - write to db
+    # to do - consolidate with app.py?
+    return request.get_json()
+
 
 if __name__ == '__main__':
     with app.app_context():
