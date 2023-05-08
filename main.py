284f93891326c74bb10f38982a10319152d3dd55
Jody Broad <msjodybroad@gmail.com>
2023-05-08 14:04:42 +0100

    added table relationships and added basic dummy data

--- main.py
+++ main.py
@@ -24,40 +24,52 @@ db = SQLAlchemy(app)
 
 # UserInfo table
 class UserInfo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     forename = db.Column(db.String(50), nullable=False)
     surname = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), nullable=False)
-
-
-class DailyRecord(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    recordDate = db.Column(db.Date, nullable=False)
-    mood_id = db.Column(db.Integer, foreign_key=True)
-    sleep_id = db.Column(db.Integer, foreign_key=True)
-    water_intake = db.Column(db.Integer, nullable=True)
-    steps_taken = db.Integer(db.Column, nullable=True)
+    # not field in table but relationship between userInfo and DailyRecord
+    user_info = db.relationship('DailyRecord', backref='user_info')
 
 
 class MoodStatus(db.Model):
     mood_id = db.Column(db.Integer, primary_key=True)
-    mood_status = db.Column(db.String(50), nullable=False)
-
-
-class SleepRecord(db.Model):
-    sleep_id = db.Column(db.Integer, primary_key=True)
-    sleepDuration_id = db.Column(db.Integer, foreign_key=True)
-    sleepQuality_id = db.Column(db.Integer, foreign_key=True)
+    mood = db.Column(db.String(50), nullable=False)
+    # not field in table but relationship between MoodStatus and DailyRecord
+    mood_status = db.relationship('DailyRecord', backref='mood_status')
 
 
 class SleepDuration(db.Model):
-    sleepDuration_id = db.Column(db.Integer, primary_key=True)
+    sleep_duration_id = db.Column(db.Integer, primary_key=True)
     duration = db.Column(db.String(50), nullable=False)
+    # not field in table but relationship between SleepDuration and DailyRecord
+    sleep_duration = db.relationship('DailyRecord', backref='sleep_duration')
 
 
 class SleepQuality(db.Model):
-    sleepQuality_id = db.Column(db.Integer, primary_key=True)
+    sleep_quality_id = db.Column(db.Integer, primary_key=True)
     quality = db.Column(db.String(50), nullable=False)
+    # not field in table but relationship between SleepQuality and DailyRecord
+    sleep_quality = db.relationship('DailyRecord', backref='sleep_quality')
+
+
+class DailyRecord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # user_id is the Foreign Key linking to the UserInfo table
+    user_id = db.Column(db.Integer, db.ForeignKey('user_info'), nullable=False)
+    record_date = db.Column(db.Date, nullable=False)
+    # mood_id is the Foreign Key linking to the MoodStatus table
+    mood_id = db.Column(db.Integer, db.ForeignKey('mood_status'))
+    # save the mood diary free text entry to the daily record
+    mood_diary = db.Column(db.String(250), nullable=True)
+    # sleep_duration_id is the Foreign Key linking to the SleepDuration table (will be one of a choice of categories)
+    sleep_duration_id = db.Column(db.Integer, db.ForeignKey('sleep_duration'))
+    # sleep_quality_id is the Foreign Key linking to the SleepQuality table (will be one of a choice of categories)
+    sleep_quality_id = db.Column(db.Integer, db.ForeignKey('sleep_quality'))
+    water_intake = db.Column(db.Integer, nullable=True)
+    steps_taken = db.Column(db.Integer, nullable=True)
+
+
 
 
 # This should ideally be in separate create.py file but ok here for now
@@ -74,9 +86,43 @@ user_5 = UserInfo(forename='Georgie', surname='Annett', email='Georgie@email.com
 # save the rows to a list
 users = [user_1, user_2, user_3, user_4, user_5]
 
-# need to create all the other tables!
+# MoodStatus
+mood_1 = MoodStatus(mood='Happy')
+mood_2 = MoodStatus(mood='Sad')
+mood_3 = MoodStatus(mood='Angry')
+mood_4 = MoodStatus(mood='Sleepy')
+mood_5 = MoodStatus(mood='Sick')
+mood_6 = MoodStatus(mood='Anxious')
 
+# save the rows to a list
+moods = [mood_1, mood_2, mood_3, mood_4, mood_5, mood_6]
 
+# Sleep duration
+sleep_1 = SleepDuration(duration='1-5 hours')
+sleep_2 = SleepDuration(duration='5-8 hours')
+sleep_3 = SleepDuration(duration='8-10 hours')
+sleep_4 = SleepDuration(duration='11+ hours')
+
+# save the rows to a list
+sleeps = [sleep_1, sleep_2, sleep_3, sleep_4]
+
+# Sleep Quality
+quality_1 = SleepQuality(quality="Good")
+quality_2 = SleepQuality(quality="Bad")
+
+# save the rows to a list
+qualities = [quality_1, quality_2]
+
+# Daily Record
+daily_record_1 = DailyRecord(user_id=1, record_date='2023-05-08', mood_id=1, mood_diary='Fine',
+                             sleep_duration_id=3, sleep_quality_id=1, water_intake=1500, steps_taken=8500)
+daily_record_2 = DailyRecord(user_id=2, record_date='2023-05-07', mood_id=2,
+                             sleep_duration_id=2, sleep_quality_id=2, water_intake=1800, steps_taken=12000)
+daily_record_3 = DailyRecord(user_id=3, record_date='2023-05-08', mood_id=4, mood_diary='So very tired',
+                             sleep_duration_id=1, sleep_quality_id=2, water_intake=2500, steps_taken=11500)
+
+# save the rows to a list
+records = [daily_record_1, daily_record_2, daily_record_3]
 
 # This should be in separate forms.py file but ok here for now
 
@@ -131,5 +177,9 @@ if __name__ == '__main__':
         db.create_all()
         # use this to add the list of users all at once (will add more for other tables)
         db.session.add_all(users)
+        db.session.add_all(moods)
+        db.session.add_all(sleeps)
+        db.session.add_all(qualities)
+        db.session.add_all(records)
         db.session.commit()
     app.run(debug=True)
