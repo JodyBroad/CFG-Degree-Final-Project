a29a83ff76fd199240ea08a0fd26729cef2389a4
Jody Broad <msjodybroad@gmail.com>
2023-05-08 21:09:46 +0100

    tracking page, form, routes now works - default values of today's date and user_id = 1 until we have log in and date selector working on home page also added "no data" fields for Mood, Sleep Duration and Sleep Quality so user does not have to enter data for these fields and updated all the tables accordingly

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 # this is what we will use to capture data entered on the website
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, RadioField, SelectField, IntegerField
 # validators will be useful for making sure valid input coming from the website
 from wtforms.validators import Email, DataRequired
+# for date info
+from datetime import datetime
 
 # creating an instance of the app
 app = Flask(__name__)
@@ -86,39 +88,42 @@ user_5 = UserInfo(forename='Georgie', surname='Annett', email='Georgie@email.com
 users = [user_1, user_2, user_3, user_4, user_5]
 
 # MoodStatus
-mood_1 = MoodStatus(mood='Happy')
-mood_2 = MoodStatus(mood='Sad')
-mood_3 = MoodStatus(mood='Angry')
-mood_4 = MoodStatus(mood='Sleepy')
-mood_5 = MoodStatus(mood='Sick')
-mood_6 = MoodStatus(mood='Anxious')
+mood_1 = MoodStatus(mood='No data')
+mood_2 = MoodStatus(mood='Happy')
+mood_3 = MoodStatus(mood='Sad')
+mood_4 = MoodStatus(mood='Angry')
+mood_5 = MoodStatus(mood='Sleepy')
+mood_6 = MoodStatus(mood='Sick')
+mood_7 = MoodStatus(mood='Anxious')
 
 # save the rows to a list
-moods = [mood_1, mood_2, mood_3, mood_4, mood_5, mood_6]
+moods = [mood_1, mood_2, mood_3, mood_4, mood_5, mood_6, mood_7]
 
 # Sleep duration
-sleep_1 = SleepDuration(duration='1-5 hours')
-sleep_2 = SleepDuration(duration='5-8 hours')
-sleep_3 = SleepDuration(duration='8-10 hours')
-sleep_4 = SleepDuration(duration='10+ hours')
+sleep_1 = SleepDuration(duration='No data')
+sleep_2 = SleepDuration(duration='1-5 hours')
+sleep_3 = SleepDuration(duration='5-8 hours')
+sleep_4 = SleepDuration(duration='8-10 hours')
+sleep_5 = SleepDuration(duration='10+ hours')
 
 # save the rows to a list
-sleeps = [sleep_1, sleep_2, sleep_3, sleep_4]
+sleeps = [sleep_1, sleep_2, sleep_3, sleep_4, sleep_5]
 
 # Sleep Quality
-quality_1 = SleepQuality(quality="Good")
-quality_2 = SleepQuality(quality="Bad")
+quality_1 = SleepQuality(quality="No data")
+quality_2 = SleepQuality(quality="Good")
+quality_3 = SleepQuality(quality="Bad")
 
 # save the rows to a list
-qualities = [quality_1, quality_2]
+qualities = [quality_1, quality_2, quality_3]
 
 # Daily Record
-daily_record_1 = DailyRecord(user_id=1, record_date='2023-05-08', mood_id=1, mood_diary='Fine',
-                             sleep_duration_id=3, sleep_quality_id=1, water_intake=1500, steps_taken=8500)
-daily_record_2 = DailyRecord(user_id=2, record_date='2023-05-07', mood_id=2,
-                             sleep_duration_id=2, sleep_quality_id=2, water_intake=1800, steps_taken=12000)
-daily_record_3 = DailyRecord(user_id=3, record_date='2023-05-08', mood_id=4, mood_diary='So very tired',
-                             sleep_duration_id=1, sleep_quality_id=2, water_intake=2500, steps_taken=11500)
+daily_record_1 = DailyRecord(user_id=1, record_date='2023-05-08', mood_id=2, mood_diary='Fine',
+                             sleep_duration_id=3, sleep_quality_id=2, water_intake=1500, steps_taken=8500)
+daily_record_2 = DailyRecord(user_id=2, record_date='2023-05-07', mood_id=3,
+                             sleep_duration_id=3, sleep_quality_id=3, water_intake=1800, steps_taken=12000)
+daily_record_3 = DailyRecord(user_id=3, record_date='2023-05-08', mood_id=5, mood_diary='So very tired',
+                             sleep_duration_id=2, sleep_quality_id=3, water_intake=2500, steps_taken=11500)
 
 # save the rows to a list
 records = [daily_record_1, daily_record_2, daily_record_3]
@@ -135,6 +140,25 @@ class BasicRegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email(message="Please supply a valid email")])
     password = StringField('Password', validators=[DataRequired()])
     submit = SubmitField('Register')
+
+
+class TrackingForm(FlaskForm):
+    # date is defaulting to today's date for now - can get this pulling through the date entered on the home screen later
+    # date = datetime.today().strftime('%Y-%m-%d')
+    mood_id = RadioField('Select the emoji that most closely matches your mood', choices=[(1, 'No data'), (2, 'Happy'),
+                                                                                          (3, 'Sad'), (4, 'Angry'),
+                                                                                          (5, 'Sleepy'), (6, 'Sick'),
+                                                                                          (7, 'Anxious')], default=1,
+                                                                                            coerce=int)
+    mood_diary = StringField('Add a short optional reflective diary entry')
+    sleep_duration_id = SelectField('How many hours did you sleep?', choices=[(1, 'No data'), (2, '1-5 hours'),
+                                                                              (3, '5-8 hours'), (4, '8-10 hours'),
+                                                                              (5, '10+ hours')])
+    sleep_quality_id = SelectField('How would you rate the quality of your sleep?', choices=[(1, 'No data'),
+                                                                                             (2, 'Good'), (3, 'Bad')])
+    water_intake = IntegerField('How many ml of water have you drunk today?')
+    steps_taken = IntegerField('How many steps have you taken today?')
+    submit = SubmitField('Submit tracking record')
 
 
 # This should be in separate routes.py file but ok here for now
@@ -161,6 +185,35 @@ def home():
             flash(f'Success! {form.email.data} is now signed up for an account', 'success')
             return render_template('home.html', form=form, message=error, title='home')
     return render_template('home.html', form=form, message=error, title='home')
+
+
+@app.route('/tracking', methods=['GET', 'POST'])
+def tracking():
+    error = ""
+    form = TrackingForm()
+    if request.method == 'POST':
+        # date is defaulting to today's date for now - can get this pulling through the date entered on the home screen
+        # later
+        record_date = datetime.today().strftime('%Y-%m-%d')
+        # defaulting the user id until we have the log in working
+        user_id = 1
+        mood_id = form.mood_id.data
+        mood_diary = form.mood_diary.data
+        sleep_duration_id = form.sleep_duration_id.data
+        sleep_quality_id = form.sleep_quality_id.data
+        water_intake = form.water_intake.data
+        steps_taken = form.steps_taken.data
+        new_daily_record = DailyRecord(user_id=user_id, record_date=record_date, mood_id=mood_id, mood_diary=mood_diary,
+                                       sleep_duration_id=sleep_duration_id, sleep_quality_id=sleep_quality_id,
+                                       water_intake=water_intake, steps_taken=steps_taken)
+        db.session.add(new_daily_record)
+        db.session.commit()
+        flash(f'Success! Your entry was added', 'success')
+        # if successful, return to the homepage
+        return render_template('tracking.html', form=form, message=error, title='tracking')
+    return render_template('tracking.html', form=form, message=error, title='tracking')
+
+
 
 
 # getting list of current users
