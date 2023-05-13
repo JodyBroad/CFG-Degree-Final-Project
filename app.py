ef5e222025bbd3a7641f78c70ad0e84387b0b13a
mellon11 <melissajlong5@gmail.com>
2023-05-14 00:43:54 +0100

    split more code into modules

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from extensions import db
+from create import seed
+
+def register_extensions(app):
+    db.init_app(app)
+
+def create_app():
+    app = Flask(__name__)
+
+    # You will need to change the password here if your MySQL password is different
+    # the very first time you use this, make sure that you have gone into MySQL and just created a DB called
+    # "CFGFinalProject" - you only need to do this once, don't need to do anything else in MySQL
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@localhost/CFGFinalProject"
+    app.config['SECRET_KEY'] = "random string"
+
+    register_extensions(app)
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    with app.app_context():
+        seed()
+        from views import *
+    app.run(debug=True)
+
+# https://stackoverflow.com/questions/42909816/can-i-avoid-circular-imports-in-flask-and-sqlalchemy
---
+++ extensions.py
@@ -0,0 +1,5 @@
+# SQLAlchemy is how we link to the db
+from flask_sqlalchemy import SQLAlchemy
+
+# creating a db accessible via the app
+db = SQLAlchemy()
