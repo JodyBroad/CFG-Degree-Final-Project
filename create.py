19c93ae24691901cdca1387024960afba8b66a14
Jody Broad <msjodybroad@gmail.com>
2023-05-14 20:12:19 +0100

    adding some extra data - 5 records per user, user_id has 10 records. Also tweaked the query to display all data and sort by date of the record

--- create.py
+++ create.py
@@ -46,17 +46,73 @@ qualities = [quality_1, quality_2, quality_3]
 # Daily Record
 daily_record_1 = DailyRecord(user_id=1, record_date='2023-05-08', mood_id=2, mood_diary='Fine',
                              sleep_duration_id=3, sleep_quality_id=2, water_intake=1500, steps_taken=8500)
-daily_record_2 = DailyRecord(user_id=2, record_date='2023-05-07', mood_id=3,
+daily_record_2 = DailyRecord(user_id=1, record_date='2023-05-09', mood_id=3,
                              sleep_duration_id=3, sleep_quality_id=3, water_intake=1800, steps_taken=12000)
-daily_record_3 = DailyRecord(user_id=3, record_date='2023-05-08', mood_id=5, mood_diary='So very tired',
+daily_record_3 = DailyRecord(user_id=1, record_date='2023-05-10', mood_id=5, mood_diary='So very tired',
                              sleep_duration_id=2, sleep_quality_id=3, water_intake=2500, steps_taken=11500)
-daily_record_4 = DailyRecord(user_id=3, record_date='2023-05-07', mood_id=4, mood_diary='This is fine',
+daily_record_4 = DailyRecord(user_id=1, record_date='2023-05-11', mood_id=4, mood_diary='This is fine',
                              sleep_duration_id=5, sleep_quality_id=2, water_intake=2000, steps_taken=11500)
-daily_record_5 = DailyRecord(user_id=3, record_date='2023-05-06', mood_id=3, sleep_duration_id=4, sleep_quality_id=2,
+daily_record_5 = DailyRecord(user_id=1, record_date='2023-05-12', mood_id=3, sleep_duration_id=4, sleep_quality_id=2,
                              water_intake=1500, steps_taken=1000)
+daily_record_6 = DailyRecord(user_id=2, record_date='2023-05-08', mood_id=4, mood_diary='Fine',
+                             sleep_duration_id=3, sleep_quality_id=2, water_intake=2500, steps_taken=9500)
+daily_record_7 = DailyRecord(user_id=2, record_date='2023-05-09', mood_id=3,
+                             sleep_duration_id=3, sleep_quality_id=3, water_intake=1800, steps_taken=1000)
+daily_record_8 = DailyRecord(user_id=2, record_date='2023-05-10', mood_id=2, mood_diary='Had a great day!',
+                             sleep_duration_id=2, sleep_quality_id=3, water_intake=2500, steps_taken=11500)
+daily_record_9 = DailyRecord(user_id=2, record_date='2023-05-12', mood_id=4, mood_diary='Work sucked today!',
+                             sleep_duration_id=5, sleep_quality_id=2, water_intake=2000, steps_taken=11500)
+daily_record_10 = DailyRecord(user_id=2, record_date='2023-05-13', mood_id=3, sleep_duration_id=4, sleep_quality_id=2,
+                              water_intake=1500, steps_taken=1000)
+daily_record_11 = DailyRecord(user_id=3, record_date='2023-05-08', mood_id=2, mood_diary='Fine',
+                              sleep_duration_id=3, sleep_quality_id=2, water_intake=1500, steps_taken=8500)
+daily_record_12 = DailyRecord(user_id=3, record_date='2023-05-09', mood_id=3,
+                              sleep_duration_id=3, sleep_quality_id=3, water_intake=1800, steps_taken=12000)
+daily_record_13 = DailyRecord(user_id=3, record_date='2023-05-10', mood_id=5, mood_diary='So very tired',
+                              sleep_duration_id=2, sleep_quality_id=3, water_intake=2500, steps_taken=11500)
+daily_record_14 = DailyRecord(user_id=3, record_date='2023-05-11', mood_id=4, mood_diary='This is fine',
+                              sleep_duration_id=5, sleep_quality_id=2, water_intake=2000, steps_taken=11500)
+daily_record_15 = DailyRecord(user_id=3, record_date='2023-05-12', mood_id=3, sleep_duration_id=4, sleep_quality_id=2,
+                              water_intake=1500, steps_taken=1000)
+daily_record_16 = DailyRecord(user_id=4, record_date='2023-05-08', mood_id=4, mood_diary='Fine',
+                              sleep_duration_id=3, sleep_quality_id=2, water_intake=2500, steps_taken=9500)
+daily_record_17 = DailyRecord(user_id=4, record_date='2023-05-09', mood_id=3,
+                              sleep_duration_id=3, sleep_quality_id=3, water_intake=1800, steps_taken=1000)
+daily_record_18 = DailyRecord(user_id=4, record_date='2023-05-10', mood_id=2, mood_diary='Had a great day!',
+                              sleep_duration_id=2, sleep_quality_id=3, water_intake=2500, steps_taken=11500)
+daily_record_19 = DailyRecord(user_id=4, record_date='2023-05-12', mood_id=4, mood_diary='Work sucked today!',
+                              sleep_duration_id=5, sleep_quality_id=2, water_intake=2000, steps_taken=11500)
+daily_record_20 = DailyRecord(user_id=4, record_date='2023-05-13', mood_id=3, sleep_duration_id=4, sleep_quality_id=2,
+                              water_intake=1500, steps_taken=1000)
+daily_record_21 = DailyRecord(user_id=5, record_date='2023-05-05', mood_id=3, sleep_duration_id=4, sleep_quality_id=2,
+                              water_intake=1500, steps_taken=1000)
+daily_record_22 = DailyRecord(user_id=5, record_date='2023-05-06', mood_id=2, mood_diary='Fine',
+                              sleep_duration_id=3, sleep_quality_id=2, water_intake=1500, steps_taken=8500)
+daily_record_23 = DailyRecord(user_id=5, record_date='2023-05-07', mood_id=3,
+                              sleep_duration_id=3, sleep_quality_id=3, water_intake=1800, steps_taken=12000)
+daily_record_24 = DailyRecord(user_id=5, record_date='2023-05-08', mood_id=5, mood_diary='So very tired',
+                              sleep_duration_id=2, sleep_quality_id=3, water_intake=2500, steps_taken=11500)
+daily_record_25 = DailyRecord(user_id=5, record_date='2023-05-09', mood_id=4, mood_diary='This is fine',
+                              sleep_duration_id=5, sleep_quality_id=2, water_intake=2000, steps_taken=11500)
+daily_record_26 = DailyRecord(user_id=5, record_date='2023-05-10', mood_id=3, sleep_duration_id=4, sleep_quality_id=2,
+                              water_intake=1500, steps_taken=1000)
+daily_record_27 = DailyRecord(user_id=5, record_date='2023-05-11', mood_id=4, mood_diary='Fine',
+                              sleep_duration_id=3, sleep_quality_id=2, water_intake=2500, steps_taken=9500)
+daily_record_28 = DailyRecord(user_id=5, record_date='2023-05-12', mood_id=3,
+                              sleep_duration_id=3, sleep_quality_id=3, water_intake=1800, steps_taken=1000)
+daily_record_29 = DailyRecord(user_id=5, record_date='2023-05-13', mood_id=2, mood_diary='Had a great day!',
+                              sleep_duration_id=2, sleep_quality_id=3, water_intake=2500, steps_taken=11500)
+daily_record_30 = DailyRecord(user_id=5, record_date='2023-05-14', mood_id=4, mood_diary='Work sucked today!',
+                              sleep_duration_id=5, sleep_quality_id=2, water_intake=2000, steps_taken=11500)
+
 
 # save the rows to a list
-records = [daily_record_1, daily_record_2, daily_record_3, daily_record_4, daily_record_5]
+records = [daily_record_1, daily_record_2, daily_record_3, daily_record_4, daily_record_5, daily_record_6,
+           daily_record_7, daily_record_8, daily_record_9, daily_record_10, daily_record_11, daily_record_12,
+           daily_record_13, daily_record_14, daily_record_15, daily_record_16, daily_record_17, daily_record_18,
+           daily_record_19, daily_record_20, daily_record_21, daily_record_22,
+           daily_record_23, daily_record_24, daily_record_25, daily_record_26, daily_record_27, daily_record_28,
+           daily_record_29, daily_record_30]
 
 
 def seed():
